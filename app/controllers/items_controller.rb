0ed7dd0ecf880d23bd7d3356720e8fcbd41382a8
Leandro López (inkel) <inkel.ar@gmail.com>
2011-01-22 17:37:18 -0300

    Add item to a list.
    If the item doesn't exists it gets created and associated to the list.
    If the article doesn't exists it gets created.
    If the item already exists in the list updates the quantity.

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -22,8 +22,44 @@ class ItemsController < ApplicationController
     end
   end
 
+  #
+  # Didn't aliased it to #add because I tried it and didn't worked, and I
+  # don't really know what that method does.
+  # And the logic seems quite simplistic :P
+  def create
+    @list = List.find(params[:item][:list_id])
+
+    article_id = unless params[:article][:name].blank?
+                   article = Article.find_by_name(params[:article][:name])
+                   if article.nil?
+                     Article.create!(params[:article]).id
+                   else
+                     article.id
+                   end
+                 else
+                   Article.find(params[:item][:article_id])
+                 end
+
+    @item = @list.items.find_by_article_id(article_id)
+
+    if @item.nil?
+      @item = Item.create(params[:item].merge({ :article_id => article_id }))
+    else
+      @item.quantity = @item.quantity + params[:item][:quantity].to_i
+      # TODO: Update the price? Raise an error?
+      # @item.price    = @item.price    + params[:item][:price].to_f
+    end
+
+    if @item.save
+      redirect_to(list_path(@list), :notice => "Added #{@item.quantity} x #{@item.article.name}")
+    else
+      render :action => "edit"
+    end
+  end
+
   def edit
     @item = Item.find(params[:id])
+    @list = @item.list
   end
 
   def update
