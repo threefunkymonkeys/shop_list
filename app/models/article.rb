e733c83ae0d6a347b0cde0f2965b2d9cc2ed570c
Leandro López (inkel) <inkel.ar@gmail.com>
2011-02-05 14:18:19 -0300

    Fix method arity exception

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,6 +2,6 @@ class Article < ActiveRecord::Base
   validates :name, :uniqueness => true, :presence => true
 
   scope :for_list, lambda { |list|
-    select(:id, :name).where("id NOT IN (?)", list.items.map(&:article_id)) if list.items.any?
+    select([:id, :name]).where("id NOT IN (?)", list.items.map(&:article_id)) if list.items.any?
   }
 end
