cc899fa4ba86a6fbb0eabe1236cd0ab5d913dcde
Leandro López (inkel) <inkel.ar@gmail.com>
2011-03-18 19:21:46 -0300

    Improve list spec

--- app/models/list.rb
+++ app/models/list.rb
@@ -4,10 +4,23 @@ class List < ActiveRecord::Base
   belongs_to :user
   has_many :items
 
-  accepts_nested_attributes_for :items, :allow_destroy => true, 
+  accepts_nested_attributes_for :items, :allow_destroy => true,
     :reject_if => proc { |attrs| attrs.all? { |k, v| v.blank? } }
 
   def total
-    items.inject(0) { |total, item| total + item.quantity * item.price }
+    items.inject(0) { |sum, item| sum + item.quantity * item.price }
+  end
+
+  def << new_item
+    item = items.find_by_article_id(new_item.article_id)
+
+    if item.nil?
+      item = new_item
+    else
+      item.quantity += new_item.quantity
+    end
+
+    item.save
+    items << item
   end
 end
--- spec/models/list_spec.rb
+++ spec/models/list_spec.rb
@@ -4,6 +4,9 @@ describe List do
   it "should have a name" do
     list = List.new
     list.should_not be_valid
+
+    list.name = "Groceries"
+    list.should be_valid
   end
 
   it "should have a total of zero if is has no items" do
@@ -11,5 +14,34 @@ describe List do
     list.total.should == 0
   end
 
-  it "should sum the individual items prices in order to get the total"
+  it "should associate the item list when item is added" do
+    item = Summoner.summon :item, :list => nil
+    list = Summoner.summon :list
+
+    list << item
+    item.list.should == list
+  end
+
+  it "should compute the total from items' quantities and prices" do
+    list  = Summoner.summon :list
+    item1 = Summoner.summon :item, :price => 10, :quantity => 1, :article_id => 1
+    item2 = Summoner.summon :item, :price => 15, :quantity => 2, :article_id => 2
+
+    list << item2
+    list.total.should == 30
+
+    list << item1
+    list.total.should == 40
+  end
+
+  it "should increment the quantity of an item if added more than once" do
+    list  = Summoner.summon :list
+    item = Summoner.summon :item, :price => 10, :quantity => 1
+
+    list << item
+    list.items.first.quantity.should == 1
+
+    list << item
+    list.items.first.quantity.should == 2
+  end
 end
