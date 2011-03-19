c0750ee63ab9b949cbd416dcca56dffce1606c04
Leonardo Mateo <leonardomateo@gmail.com>
2011-03-19 16:36:51 -0300

    Basic menu layout and behavior

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,16 @@ module ApplicationHelper
   def sanitize_partial(html)
     html.gsub("\"", "'").gsub("\n", "").strip if html
   end
+
+  def select_for_controller(controller_name, action_name)
+   return 'selected' if controller_name.to_sym == controller.params[:controller].to_sym and action_name.to_sym == controller.params[:action].to_sym
+  end
+
+  def current_menu_title
+    I18n.translate ".#{controller.params[:controller]}.#{controller.params[:action]}.menu_name"
+  end
+
+  def show_current_menu
+    controller.params[:action] == "edit" || controller.params[:action] == "new"
+  end
 end
