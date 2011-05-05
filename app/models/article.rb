67305d8a838a26775dcf5b2f6fd188d6fd4f742a
Leonardo Mateo <leonardomateo@gmail.com>
2011-05-05 00:21:34 -0300

    Add validation user scope for article name

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,7 +1,8 @@
 class Article < ActiveRecord::Base
   belongs_to :user
 
-  validates :name, :uniqueness => true, :presence => true
+  validates :name, :presence => true
+  validates_uniqueness_of :name, :scope => :user_id
 
   scope :for_list, lambda { |list|
     select([:id, :name]).where("id NOT IN (?)", list.items.map(&:article_id)) if list.items.any?
